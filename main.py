df924b3a0061e7a9d6a25429eecc7a4e5d3417d5
Thomas Fiorilla <engineertf@gmail.com>
2020-02-13 17:48:03 -0500

    Initial Work on AI
    Started work for the breadth first search

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import board				#import code for creating the board
-import draw					#import code for visually displaying the board on screen
+import board				# import code for creating the board
+import draw					# import code for visually displaying the board on screen
 
 def main():
-	gameBoard = board.Board()					# builds a random board and sets the return values equal to boardArray (holds the array) and boardSize (holds the size)
-	draw.drawBoard(gameBoard.boardBuilt, gameBoard.boardSize)						# takes the created board and draws it with turtle, as visual output
+	gameBoard = board.Board()											# builds a Board() object and assigns it to gameBoard
+	draw.drawBoard(gameBoard.boardBuilt, gameBoard.boardSize)			# takes the created board and draws it with turtle, as visual output
 	
-	input("All done!")
+	input("Initial game board displayed.")
+	draw.turtle.clear()
 #
 
 
