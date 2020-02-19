99b7fdbcab6ae0c0eb4479d62b7057ccbdc2c8a8
Thomas Fiorilla <engineertf@gmail.com>
2020-02-19 12:00:47 -0500

    Fixes hill algorithm
    Correctly implements hill climbing as a class.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Thomas Fiorilla
 # Main file to execute the code.
+# This version is made for Python 3; mainAT.py is for Python 2.
 
 import board				# import code for creating the board
 import draw					# import code for visually displaying the board on screen
@@ -11,18 +12,19 @@ def main():
 	
 	gameBoard = board.Board(userInput)											# builds a Board() object and assigns it to gameBoard
 	
-	bfs = evaluate.evaluate(gameBoard.boardBuilt, gameBoard.boardSize)		# creates the bfs object
-	hillBoard = hill.Hill(gameBoard)
+	bfs = evaluate.evaluate(gameBoard.boardBuilt, gameBoard.boardSize)		# creates the bfs object for the original board
+	hillBoard = hill.Hill(gameBoard)										# creates a hill-climbing object with the original board
 	
-	print("Score for the board: " + str(bfs.value))
+	print("Score for the board: " + str(bfs.value))							# prints the score for the original board
 	
-	x = 0
-	while x == 0:
-		userInput = input("Type '1' for the board, '2' for number of steps, '3' to implement Hill Climbing, '4' to view the Hill Board, anything else to exit. ")
+	# create a loop for user input to make selections to view the original board, etc
+	while True:
+		print("Type '1' for the original board, '2' for number of steps on original board, '3' to implement Hill Climbing, '4' to view the Hill Board,")
+		userInput = input("    '5' to view steps on Hill Board, anything else to exit. ")
 		
 		if userInput == '1':
 			draw.drawBoard(gameBoard.boardBuilt, gameBoard.boardSize)			# takes the created board and draws it with turtle, as visual output
-			print("> Initial game board displayed.")
+			print("> Original game board displayed.")
 			print()
 		
 		elif userInput == '2':
@@ -36,17 +38,23 @@ def main():
 			while userInput < 0:
 				userInput = input("> How many iterations would you like the hill-climbing to try? ")
 			
-			hillBoard.climb(gameBoard, userInput)
+			hillBoard.climb(userInput)
 			
 			print("> New board score: " + str(hillBoard.score))
 		
 		elif userInput == '4':
-			draw.drawBoard(hillBoard.newPuzzle.boardBuilt, gameBoard.boardSize)			# takes the created board and draws it with turtle, as visual output
+			draw.drawBoard(hillBoard.puzzle.boardBuilt, gameBoard.boardSize)			# takes the created board and draws it with turtle, as visual output
 			print("> Hill-CLimbing game board displayed.")
 			print()
 		
+		elif userInput == '5':
+			hillBFS = evaluate.evaluate(hillBoard.puzzle.boardBuilt, hillBoard.puzzle.boardSize)			# creates a new bfs object for the hill-climbing board
+			draw.drawBoard(hillBFS.steps, hillBoard.puzzle.boardSize)
+			print("> Hill-Climbing board number of steps displayed")
+			print()
+		
 		else:
-			x = -1
+			break
 #
 
 
