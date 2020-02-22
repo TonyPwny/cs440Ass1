65fc0152a1f59f646058ed64ebad4dcf2340ea69
Anthony Tiongson <anthonytiongson@me.com>
2020-02-21 20:16:27 -0500

    dear lord i think i finally figured out A*

--- AStarEval.py
+++ AStarEval.py
@@ -3,7 +3,7 @@
 
 from queue import PriorityQueue
 import time
-# f(n) = total moves to goal where goal is when i = j = size - 1
+# f(n) = estimated total moves to goal where goal is when i = j = size - 1
 # f(n) = g(n) + h(n)
 # g(n) = moves so far
 # h(n) = heuristic estimating how many moves to goal, never overestimate
@@ -20,59 +20,39 @@ class AStarEval():
 		self.steps = [['X' for i in range(size)] for j in range(size)]
 
 		pQ = PriorityQueue()
+		v = set()
 		prevPos = {}
-		moves = {}
 
-		pQ.put((0, 0), 0)
-		prevPos[(0, 0)] = None
-		moves[(0, 0)] = 0
+		pQ.put((2, 0, 0, 0))
+		v.add((0, 0))
+		prevPos[(0,0)] = None
 
 		while not pQ.empty():
-			i, j = pQ.get()
+			estimate, i, j, depth = pQ.get()
+
 			print('Current position: (' + str(i) + ', ' + str(j) + ')')
 			print('Previous position: ' + str(prevPos[(i, j)]))
 
 			self.visited[i][j] = 'Y'
-			self.steps[i][j] = moves[(i, j)]
+			self.steps[i][j] = depth
 
 			if i == size - 1 and j == size - 1:
 				break
 
 			m = board[i][j]
 
-			# for ALL possible positions i_2, j_2 from the current position,
-			# 	including invalid ones
 			for i_2, j_2 in ((i-m, j), (i+m, j), (i, j-m), (i, j+m)):
 
-				# if the next subsequent coordinate i_2, j_2 is valid with respect
-				#	to the size of the puzzle board
-				if (0 <= i_2 < size) and (0 <= j_2 < size) and (i_2, j_2) not in moves:
+				if (0 <= i_2 < size) and (0 <= j_2 < size) and ((i_2, j_2) not in v):
 
-					moves[(i_2, j_2)] = moves[(i, j)] + 1
-					m_2 = board[i_2][j_2]
-
-					if (i_2 == size - 1 and i_2 == size - 1):
-						heuristic = 0
-					elif (i_2 == size - 1 or j_2 == size - 1):
-						heuristic = 1
+					if i_2 == size - 1 or j_2 == size - 1:
+						estimate = depth + 1
 					else:
-						heuristic = 2
+						estimate = depth + 2
 
-					for i_3, j_3 in ((i_2-m_2, j_2), (i_2+m_2, j_2), (i_2, j_2-m_2), (i_2, j_2+m_2)):
-
-						if (0 <= i_2 < size) and (0 <= j_2 < size) and (i_2, j_2):
-
-							if (i_3 == size - 1 and i_3 == size - 1):
-								heuristic_2 = 0
-							elif (i_3 == size - 1 or j_3 == size - 1):
-								heuristic_2 = 1
-							else:
-								heuristic_2 = 2
-
-							priority = moves[(i_2, j_2)] + heuristic + heuristic_2
-							pQ.put((i_2, j_2), priority)
-							prevPos[(i_2, j_2)] = (i, j)
-
+					pQ.put((estimate, i_2, j_2, self.steps[i][j] + 1))
+					v.add((i_2, j_2))
+					prevPos[(i_2, j_2)] = (i, j)
 
 		if self.visited[size - 1][size - 1] != 'N':
 			self.value = self.steps[size - 1][size - 1]
@@ -82,7 +62,8 @@ class AStarEval():
 				k -= n.count('N')
 			self.value = k
 
-		print('A* evaluation done: solved in ' + str(moves[(size - 1, size - 1)]))
+		print('A* evaluation done: solved in ' + str(self.steps[size - 1][size - 1]) + ' moves')
+		print('A* value is ' + str(self.value))
 
 		end = time.time()
 
--- evaluate.py
+++ evaluate.py
@@ -13,23 +13,22 @@ class evaluate():
 
 		q = Queue()
 		v = set()
-		depth = 0
 
-		q.put((0, 0, depth))
+		q.put((0, 0, 0))
 		v.add((0, 0))
 
 		while not q.empty():
-			i, j, d = q.get()
-			
+			i, j, depth = q.get()
+
 			m = board[i][j]
 			self.visited[i][j] = 'Y'
-			self.steps[i][j] = d
+			self.steps[i][j] = depth
 			for i_2, j_2 in ((i-m, j), (i+m, j), (i, j-m), (i, j+m)):
-				
+
 				if (0 <= i_2 < size) and (0 <= j_2 < size) and ((i_2, j_2) not in v):
 					q.put((i_2, j_2, self.steps[i][j] + 1))
 					v.add((i_2, j_2))
-					
+
 		if self.visited[size - 1][size - 1] != 'N':
 			self.value = self.steps[size - 1][size - 1]
 		else:
